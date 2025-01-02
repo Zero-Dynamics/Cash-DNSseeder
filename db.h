6e9460cc4a0c13bf6d59efd5c42aa2990c7f3409
Herbert Jr. <hml.sanders@outlook.com>
2025-01-02 23:48:52 +0100

    Add RequiredHeight

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 0;
+    return testnet ? 0 : 723600;
 }
 
 std::string static inline ToString(const CService &ip) {
